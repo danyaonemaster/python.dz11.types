863fbfbd0740f9b284e62cdd88b67155e736f775
danyaonemaster <157752827+danyaonemaster@users.noreply.github.com>
2024-02-27 12:09:21 +0200

    added task_03.py

--- task_03.py
+++ task_03.py
@@ -2,15 +2,21 @@ input_login = input("Enter your login : ")
 login_u = ("admin", "user", "QA", "Dev", "superuser", "superadmin")
 pass_u = (125, 124, 354, 781, 874, 347)
 
-
 if input_login in login_u:
-    input_password = int(input("Enter your password : "))
-    if login_u.index(input_login) == pass_u.index(input_password):
-        exit(f"""
-    login : {input_login}
-    password : {input_password}
-    Access""")
+    index = login_u.index(input_login)
+    if input_login == login_u[index]:
+        input_password = int(input("Enter your password : "))
+        if input_password == pass_u[index]:
+            print(f"""
+        login : {input_login}
+        password : {input_password}
+        Access""")
+        else:
+            print(f"""
+        login : {input_login}
+        password : {input_password}
+        Not Access""")
 else:
-    exit(f"""
+    print(f"""
     login : {input_login}
     Not Access""")
