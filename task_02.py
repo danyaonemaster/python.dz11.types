c26b2faa962ddc77c4322fe69ef91cf99ff2190b
danyaonemaster <157752827+danyaonemaster@users.noreply.github.com>
2024-02-27 20:06:00 +0200

    added task_02.py

--- task_02.py
+++ task_02.py
@@ -1,12 +1,13 @@
 tuples = (2, 8, 10, 25, 1, 2, 771, 125, 256, 100, 8, 10, 81, 96, 8, 1, 144, 5)
-len_one_digit = len([i for i in tuples if len(str(i)) == 1])
-len_two_digit = len([i for i in tuples if len(str(i)) == 2])
-len_three_digit = len([i for i in tuples if len(str(i)) == 3])
+lists = []
+list_str = ["One", "Two", "Three"]
 
 print(f"""max number : {max(tuples)}
 min number : {min(tuples)}
 sum tuple : {sum(tuples)}
-arithmetic mean : {round(sum(tuples) / len(tuples), 2)}
-One number - {len_one_digit} element
-Two numbers - {len_two_digit} element
-Three numbers - {len_three_digit} elements""")
+arithmetic mean : {round(sum(tuples) / len(tuples), 2)}""")
+
+for index, number in zip(range(1, 4), list_str):
+    lists.append([i for i in tuples if len(str(i)) == index])
+    print(f"{number} number - {len(lists[index - 1])} elements")
+
